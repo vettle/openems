cba1cdc768983de2a7eced92bb62baffc7443d7d
Hüseyin Sahutoglu <hueseyin.sahutoglu@fenecon.de>
2019-04-29 09:22:48 +0200

    Initialize power sum channel and convert from Doubleword to word

--- io.openems.edge.meter.spanner/src/io/openems/edge/meter/spanner/MeterSpannerBHKW.java
+++ io.openems.edge.meter.spanner/src/io/openems/edge/meter/spanner/MeterSpannerBHKW.java
@@ -16,8 +16,8 @@ import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
 import io.openems.edge.bridge.modbus.api.BridgeModbus;
 import io.openems.edge.bridge.modbus.api.ElementToChannelConverter;
 import io.openems.edge.bridge.modbus.api.ModbusProtocol;
-import io.openems.edge.bridge.modbus.api.element.SignedDoublewordElement;
-import io.openems.edge.bridge.modbus.api.element.UnsignedDoublewordElement;
+import io.openems.edge.bridge.modbus.api.element.SignedWordElement;
+import io.openems.edge.bridge.modbus.api.element.UnsignedWordElement;
 import io.openems.edge.bridge.modbus.api.task.FC3ReadRegistersTask;
 import io.openems.edge.common.channel.Doc;
 import io.openems.edge.common.component.OpenemsComponent;
@@ -45,7 +45,9 @@ public class MeterSpannerBHKW extends AbstractOpenemsModbusComponent
 				OpenemsComponent.ChannelId.values(), //
 				SymmetricMeter.ChannelId.values(), //
 				AsymmetricMeter.ChannelId.values(), //
-				ChannelId.values());
+				ChannelId.values()//
+		);
+		AsymmetricMeter.initializePowerSumChannels(this);
 	}
 
 	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
@@ -74,32 +76,32 @@ public class MeterSpannerBHKW extends AbstractOpenemsModbusComponent
 	protected ModbusProtocol defineModbusProtocol() {
 		return new ModbusProtocol(this, //
 				new FC3ReadRegistersTask(370, Priority.HIGH, //
-						m(new UnsignedDoublewordElement(370)) //
+						m(new UnsignedWordElement(370)) //
 								.m(AsymmetricMeter.ChannelId.VOLTAGE_L1, ElementToChannelConverter.SCALE_FACTOR_2) //
 								.m(SymmetricMeter.ChannelId.VOLTAGE, ElementToChannelConverter.SCALE_FACTOR_2) //
 								.build(), //
-						m(AsymmetricMeter.ChannelId.VOLTAGE_L2, new UnsignedDoublewordElement(372),
+						m(AsymmetricMeter.ChannelId.VOLTAGE_L2, new UnsignedWordElement(371),
 								ElementToChannelConverter.SCALE_FACTOR_2), //
-						m(AsymmetricMeter.ChannelId.VOLTAGE_L3, new UnsignedDoublewordElement(374),
+						m(AsymmetricMeter.ChannelId.VOLTAGE_L3, new UnsignedWordElement(372),
 								ElementToChannelConverter.SCALE_FACTOR_2), //
-						m(BHKWChannelId.FREQUENCY_L1, new UnsignedDoublewordElement(376),
+						m(BHKWChannelId.FREQUENCY_L1, new UnsignedWordElement(373),
 								ElementToChannelConverter.SCALE_FACTOR_1), //
-						m(BHKWChannelId.FREQUENCY_L2, new UnsignedDoublewordElement(378),
+						m(BHKWChannelId.FREQUENCY_L2, new UnsignedWordElement(374),
 								ElementToChannelConverter.SCALE_FACTOR_1), //
-						m(BHKWChannelId.FREQUENCY_L3, new UnsignedDoublewordElement(380),
+						m(BHKWChannelId.FREQUENCY_L3, new UnsignedWordElement(375),
 								ElementToChannelConverter.SCALE_FACTOR_1), //
-						m(AsymmetricMeter.ChannelId.CURRENT_L1, new UnsignedDoublewordElement(382),
+						m(AsymmetricMeter.ChannelId.CURRENT_L1, new UnsignedWordElement(376),
 								ElementToChannelConverter.SCALE_FACTOR_2), //
-						m(AsymmetricMeter.ChannelId.CURRENT_L2, new UnsignedDoublewordElement(384),
+						m(AsymmetricMeter.ChannelId.CURRENT_L2, new UnsignedWordElement(377),
 								ElementToChannelConverter.SCALE_FACTOR_2), //
-						m(AsymmetricMeter.ChannelId.CURRENT_L3, new UnsignedDoublewordElement(386),
+						m(AsymmetricMeter.ChannelId.CURRENT_L3, new UnsignedWordElement(378),
 								ElementToChannelConverter.SCALE_FACTOR_2), //
-						m(BHKWChannelId.COSPHI_L1, new SignedDoublewordElement(388)), //
-						m(BHKWChannelId.COSPHI_L1, new SignedDoublewordElement(390)), //
-						m(BHKWChannelId.COSPHI_L1, new SignedDoublewordElement(392)), //
-						m(AsymmetricMeter.ChannelId.ACTIVE_POWER_L1, new SignedDoublewordElement(394)), //
-						m(AsymmetricMeter.ChannelId.ACTIVE_POWER_L2, new SignedDoublewordElement(396)), //
-						m(AsymmetricMeter.ChannelId.ACTIVE_POWER_L3, new SignedDoublewordElement(398))) //
+						m(BHKWChannelId.COSPHI_L1, new SignedWordElement(379)), //
+						m(BHKWChannelId.COSPHI_L1, new SignedWordElement(380)), //
+						m(BHKWChannelId.COSPHI_L1, new SignedWordElement(381)), //
+						m(AsymmetricMeter.ChannelId.ACTIVE_POWER_L1, new SignedWordElement(382)), //
+						m(AsymmetricMeter.ChannelId.ACTIVE_POWER_L2, new SignedWordElement(383)), //
+						m(AsymmetricMeter.ChannelId.ACTIVE_POWER_L3, new SignedWordElement(384))) //
 		);//
 	}
 
